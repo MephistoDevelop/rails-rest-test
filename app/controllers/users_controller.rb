550db59dd94778b8cec57ad11d56845cea93d36d
Cristian <inesherald@gmail.com>
2019-08-23 12:52:34 -0500

    finish feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,16 @@ class UsersController < ApplicationController
   def index
     @name = "Soy la accion Index !!"
   end
+  def new
+    @name =  "Soy Mephistodevelop"
+  end
   def create
-
+    @name =  "Soy Mephistodevelop"
+  end
+  def update
+    @name =  "Soy Mephistodevelop"
+  end
+  def show
+    @name =  "Soy Mephistodevelop de show !!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   #root "user#hello"
+  #post "/posts", to: "users#create"
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
